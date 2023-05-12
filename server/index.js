1daa4c40ed33fdb0f60c333d12e815965807e7d5
Vasikaran <vasikaran@p2fsemi.in>
2023-05-12 11:19:27 +0530

    aws deploy

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const path =require('path')
+// const corsOptions = { 
+//   origin: "https://conference-room-booking-fe.onrender.com", // frontend URI (ReactJS)
+// }
+const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./config/db");
+const user = require("./router/user");
+InitiateMongoServer();
+app.use(express.json());
+const EventRoute = require('./router/EventRoutes');
+app.use('/',  EventRoute);
+const EventTimeSlotRoute = require('./router/EventTimeSlotRoute');
+app.use('/',EventTimeSlotRoute)
+
+const _dirname = path.dirname("")
+const builPath =path.join(_dirname , "../client/build");
+app.use(express.static(builPath))
+app.get("/*", function(req,res) {
+  res.sendFile(
+    path.join(_dirname, "../client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    }
+  );
+})
+
+
+
+// middleware
+app.use(bodyParser.json());
+// app.use(cors(corsOptions));
+
+// PORT
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+
+// router
+
+app.use("/user", user);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
