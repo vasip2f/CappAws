b0ea78baaf6dcadbd0cb4af1ed28134bfed82ef8
Vasikaran <vasikaran@p2fsemi.in>
2023-05-12 17:44:39 +0530

    aws2

--- server/config/db.js
+++ server/config/db.js
@@ -2,17 +2,19 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
 
 
-const MongoUrl = process.env.MongoUri
+// const MongoUrl = process.env.MongoUri
+
+const MongoUrl = "mongodb+srv://calendar:calendar@calendar.hikmumx.mongodb.net/?retryWrites=true&w=majority"
 
 
-const InitiateMongoServer = async ()=>{
-    try{
-        await mongoose.connect(MongoUrl,{
-            useNewUrlParser:true
+const InitiateMongoServer = async () => {
+    try {
+        await mongoose.connect(MongoUrl, {
+            useNewUrlParser: true
         });
         console.log("connected to db")
     }
-    catch(e){
+    catch (e) {
         console.log(e)
         throw e
     }
